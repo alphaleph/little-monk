5d2e9ff2c21ba0398207dc7dca85756c5edd50e2
Alpha Chau <chaua0927@gmail.com>
2018-11-13 11:43:07 -0500

    Installed mongoose dependency, Implemented "create post via REST POST"

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/posts');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback) {
+  console.log("Connection succeeded");
+});
 
 app.get('/posts', (req, res) => {
   res.send(
@@ -16,5 +25,27 @@ app.get('/posts', (req, res) => {
     }
   );
 });
+
+app.post('/posts', (req, res) => {
+  const db = req.db; /* purpose? */
+  const title = req.body.title;
+  const description = req.body.description;
+  const newPost = Post({
+    title: title,
+    description: description
+  });
+
+  newPost.save(function (err) {
+    if (err) {
+      console.error(err);
+    }
+    res.send({
+      status: true,
+      message: "Post saved successfully!"
+    });
+  });
+
+});
+
 
 app.listen(process.env.PORT || 8081);
