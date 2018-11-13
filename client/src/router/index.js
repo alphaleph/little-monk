c0c82429eb747a63a535d7582975ffff5456add8
Alpha Chau <chaua0927@gmail.com>
2018-11-13 12:26:13 -0500

    Added NewPost UI and functionality

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
 import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
     }
   ]
 })
--- client/src/services/PostsService.js
+++ client/src/services/PostsService.js
@@ -3,5 +3,8 @@ import Api from './Api'
 export default {
   fetchPosts () {
     return Api().get('posts')
+  },
+  addPost (params) {
+    return Api().post('posts', params)
   }
 }
